e55faf3864f800a16939404b9c9f8cc4f7a8bc77
Junior Stranner <Ju-stranner@outlook.com>
2023-05-26 14:53:41 -0300

    Incredible but without Database , still work at this project

--- src/App.java
+++ src/App.java
@@ -10,14 +10,18 @@ public class App {
       
         ArrayList <Cliente> clientes = new ArrayList<>();
         ArrayList<Banco>bancos=new ArrayList<>();
+    //    ArrayList<Banco> nvsBancos = new ArrayList<>();
 
         int op = 0;
 
         do{ 
             System.out.println("Banco Ju"
             +"\n 1 - Cadastrar Cliente"
-            +"\n 2 - mostrar Conta do Cliente"
-            +"\n 3 - Fazer Depósito");
+            +"\n 2 - mostrar Dados da Conta"
+            +"\n 3 - Fazer Depósito"
+            +"\n 4 - Fazer saque "
+            +"\n 5 - Realizar Tranferencia Bancária"
+            +"\n 6 - Histórico da Conta");
             op = Integer.parseInt(in.nextLine());
            
             switch(op){
@@ -25,10 +29,84 @@ public class App {
                 case 1:cadastrarClienteBanco(clientes,bancos);break;
                 case 2:mostraContaCliente(bancos,clientes);break;
                 case 3:depositarValor(bancos);break;
+                case 4:sacarDinheiro(bancos);break;
+                case 5:transferenciaBancaria(bancos);break;
+                case 6:historicoConta(bancos,clientes);break;
             }
      
         }while(op != 9);
   
+    }
+
+    private static void historicoConta(ArrayList<Banco> bancos,ArrayList<Cliente>clientes) {
+
+        System.out.println("Digite a sua Senha ");
+        String senha = in.nextLine();
+
+        for (Banco banco : bancos) {
+            if(banco.getSenha().equalsIgnoreCase(senha)){
+
+                System.out.println("\n============================================="
+                +"\nSua Conta foi encontrada "+banco.getNomeCliente()
+                +"\n=============================================");
+
+                System.out.println("\n Numero da Conta : " +banco.getNumeroConta()
+            +"\n Nome do Cliente : "+banco.getNomeCliente()
+            +"\n Histórico Saldo Atual: "+banco.getSaldo()
+            +"\n Histórico Saque : "+banco.getSaque()
+            +"\n Status da Conta : "+banco.isStatus());
+            }else{
+                System.out.println("Conta inexistente...");
+            }
+              
+        }
+    }
+
+    private static void transferenciaBancaria(ArrayList<Banco>bancos) {
+        System.out.println("Digite o id da Conta para Transferencia");
+        int idOutraConta = Integer.parseInt(in.nextLine());
+
+        for (Banco banco : bancos) {
+
+            if(banco.getNumeroConta() == idOutraConta){
+                System.out.println("\n Conta Existente!!");
+                  if(banco.isStatus()==true){
+                    System.out.println("\n Conta Encontrada !!");
+                  }
+            }
+            System.out.println("\n Conta de "+banco.getNomeCliente()
+            +"\n Digite o valor que deseja Tranferir ");
+            double tranferirValor = Double.parseDouble(in.nextLine());
+
+            if(banco.getSaldo()>tranferirValor){
+                banco.setSaldo(banco.getSaldo() - tranferirValor);
+          }else{
+              System.out.println("Erro de Saque , saldo insuficiente");break;
+          }
+
+         System.out.println("\n Saldo Atual : "+banco.getSaldo()
+          +"\n Valor que foi Sacado  : "+tranferirValor);
+          banco.setSaque(banco.getSaque()- tranferirValor);
+
+    }
+}
+
+    private static void sacarDinheiro(ArrayList<Banco>bancos) {
+         System.out.println("Digite o Valor que deseja Sacar");
+         double valorSaque = Double.parseDouble(in.nextLine());
+         for (Banco banco : bancos) {
+            
+            if(banco.getSaldo()>valorSaque){
+                  banco.setSaldo(banco.getSaldo() - valorSaque);
+            }else{
+                System.out.println("Erro de Saque , saldo insuficiente");break;
+            }
+
+           System.out.println("\n Saldo Atual : "+banco.getSaldo()
+            +"\n Valor que foi Sacado  : "+valorSaque);
+            banco.setSaque(banco.getSaque()- valorSaque);
+         } 
+
     }
 
     private static void depositarValor( ArrayList<Banco>bancos) {
@@ -72,25 +150,30 @@ public class App {
         String senhaBanco =cliente.getSenha();
        double saldo = 0;
        double saque = 0;
+       boolean status = false;
 
-       Banco banco = new Banco(nomeCliente,numeroConta,senhaBanco,saldo,saque); 
+
+       Banco banco = new Banco(nomeCliente,numeroConta,senhaBanco,saldo,saque,status); 
+       if(banco.getNumeroConta() == id){
+        banco.setStatus(true);
+       }
        bancos.add(banco);
 
     }
 
     private static void mostraContaCliente( ArrayList<Banco>bancos, ArrayList <Cliente> clientes){
        
-
       for (Banco banco : bancos) {
         for (Cliente cliente : clientes) {
-        
+
             System.out.println("\n Numero da Conta : " +banco.getNumeroConta()
             +"\n Nome do Cliente : "+banco.getNomeCliente()
             +"\n Senha do Banco  : "+banco.getSenha()
             +"\n tipo de Conta do Cliente : "+cliente.getTipoConta()
-            +"\n Saldo : "+banco.getSaldo()
-            +"\n Saque : "+banco.getSaque());
-    
+            +"\n Saldo Atual: "+banco.getSaldo()
+            +"\n Sque : "+banco.getSaque()
+            +"\n Status da Conta : "+banco.isStatus());
+            
         }
     }
     }
--- src/model/Banco.java
+++ src/model/Banco.java
@@ -5,63 +5,80 @@ public class Banco {
     private String senha;
     private double saldo;
     private double saque;
+    private boolean status;
 
 
     public Banco() {
       super();
     }
-    
 
-    public Banco(String nomeCliente, int numeroConta, String senha, double saldo, double saque) {
+    public Banco(String nomeCliente, int numeroConta, String senha, double saldo, double saque, boolean status) {
         this.nomeCliente = nomeCliente;
         this.numeroConta = numeroConta;
         this.senha = senha;
         this.saldo = saldo;
         this.saque = saque;
-    
+        this.status = status;
     }
-
 
     public String getNomeCliente() {
         return nomeCliente;
     }
 
+
     public void setNomeCliente(String nomeCliente) {
         this.nomeCliente = nomeCliente;
     }
+
 
     public int getNumeroConta() {
         return numeroConta;
     }
 
+
     public void setNumeroConta(int numeroConta) {
         this.numeroConta = numeroConta;
     }
+
 
     public String getSenha() {
         return senha;
     }
 
+
     public void setSenha(String senha) {
         this.senha = senha;
     }
+
 
     public double getSaldo() {
         return saldo;
     }
 
+
     public void setSaldo(double saldo) {
         this.saldo = saldo;
     }
+
 
     public double getSaque() {
         return saque;
     }
 
+
     public void setSaque(double saque) {
         this.saque = saque;
     }
 
-  
 
+    public boolean isStatus() {
+        return status;
+    }
+
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    
 }
